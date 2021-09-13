5726032fb0880808925509f03f96e10dd53ccd9f
Ji TANG <ji.tang@polytechnique.edu>
2021-09-13 16:50:44 +0200

    bug fixed

--- src/piece_process.py
+++ src/piece_process.py
@@ -1,7 +1,146 @@
 from scipy.interpolate import splprep, splev
-import cv2
+import cv2, operator
 import numpy as np
 import scipy.ndimage as ndimage
+
+def get_tile_corners(mask):
+    canny = cv2.Canny(mask, 100, 255, 1)
+    corners = cv2.goodFeaturesToTrack(canny, 30, 0.1, 100)
+    corners = [tuple(corner.flatten()) for corner in corners]
+    tile_center = ndimage.center_of_mass(mask)
+    tile_center = tuple(np.round(tile_center).astype(np.int))
+    
+    def distance(c1, c2):
+        return (c1[0]-c2[0]) ** 2 + (c1[1]-c2[1]) ** 2
+    
+    lt, rt, rb, lb = tile_center, tile_center, tile_center, tile_center
+    lt_d, rt_d, rb_d, lb_d = 0, 0, 0, 0
+    for corner in corners:
+        if corner[0] < tile_center[0] and corner[1] < tile_center[1] and distance(corner, tile_center) > lt_d:
+            lt = corner
+            lt_d = distance(corner, tile_center)
+        if corner[0] < tile_center[0] and corner[1] > tile_center[1] and distance(corner, tile_center) > rt_d:
+            rt = corner
+            rt_d = distance(corner, tile_center)
+        if corner[0] > tile_center[0] and corner[1] > tile_center[1] and distance(corner, tile_center) > rb_d:
+            rb = corner
+            rb_d = distance(corner, tile_center)
+        if corner[0] > tile_center[0] and corner[1] < tile_center[1] and distance(corner, tile_center) > lb_d:
+            lb = corner
+            lb_d = distance(corner, tile_center)
+    return np.array([lt, rt, rb, lb], dtype=int)
+
+def get_warp_image(img, margin=9):
+    orig_width = len(img[0])
+    orig_height = len(img)
+    target = 800
+    scale = target/orig_width if orig_height > orig_width else target/orig_height
+
+    resized = cv2.resize(img, None, fx=scale, fy=scale)
+    img_balanced = white_balance(resized)
+    blurred = blur(img_balanced)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    h,s,v = cv2.split(hsv)
+    edges = cv2.threshold(s, 15, 255, cv2.THRESH_BINARY_INV)[1]
+
+    contour_info = [(c, cv2.contourArea(c),) for c in cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]]
+    mask = np.zeros(list(map(operator.add, edges.shape, (margin*2,margin*2,0))), dtype = np.uint8)
+    for contour in contour_info:
+        mask = cv2.fillConvexPoly(mask, contour[0]+margin, (255))
+    corners = get_tile_corners(mask)
+    corners = [tuple((corner-margin)/scale) for corner in corners]
+    
+    return warpBox(img,corners)
+
+def blur(img, filter_size=9):
+    return cv2.GaussianBlur(img, (filter_size, filter_size), 0)
+
+def find_outline_rough(img):
+    blurred = blur(img)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    h,s,v = cv2.split(hsv)
+
+    edges = cv2.threshold(s, 20, 100, cv2.THRESH_BINARY_INV)[1]
+
+    contour_info = [(c, cv2.contourArea(c),) for c in cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]]
+
+    image_area = img.shape[0] * img.shape[1]  
+    max_area = 0.50
+    max_area = max_area * image_area
+
+    mask = np.ones(edges.shape, dtype = np.uint8)*255
+    for contour in contour_info:
+        if contour[1] < max_area:
+            mask = cv2.fillPoly(mask, [contour[0].reshape(contour[0].shape[0], contour[0].shape[2])], (0))
+    return mask
+
+# Color KMeans
+from sklearn.cluster import KMeans
+
+def plot_colors(hist, centroids):
+    bar = np.zeros((50, 300, 3), dtype = "uint8")
+    startX = 0
+    for (percent, color) in zip(hist, centroids):
+        endX = startX + (percent * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50), color.astype("uint8").tolist(), -1)
+        startX = endX
+    plt.figure()
+    plt.axis("off")
+    plt.imshow(bar)
+    plt.show()
+    return bar
+
+def centroid_histogram(clt):
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins = numLabels)
+    hist = hist.astype("float")
+    hist /= hist.sum()
+    return hist
+
+def kmeans_color_purifier(mask, img_balanced, n_samples=20000, n_clusters=40, t=0.9, show=True):
+    colors = []
+    for _ in range(n_samples):
+        i, j = np.random.randint(0, len(mask)), np.random.randint(0, len(mask[0]))
+        if mask[i][j] > 0:
+            colors.append(img_balanced[i][j])
+
+    c_var = np.var(colors, axis=0)
+    
+    clt = KMeans(n_clusters=n_clusters)
+    clt.fit(colors)
+    if show:
+        hist = centroid_histogram(clt)
+        bar = plot_colors(hist, clt.cluster_centers_)
+    
+    img_overlap = np.zeros(mask.shape, dtype = "uint8")
+    for c_mean in clt.cluster_centers_:
+        m = cv2.inRange(img_balanced, c_mean-0.2*np.sqrt(c_var), c_mean+0.2*np.sqrt(c_var))
+        v = np.sum((255-m == 0)*(255-mask == 0))/np.sum((255-m == 0))
+        # verify the color not in the puzzle blocks
+        if v > t:
+            img_overlap |= m
+    return img_overlap
+
+# Pieces searching and coloring
+def pieces_searching(img_overlap):
+    kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    bin_clo = cv2.dilate(img_overlap, kernel2, iterations=1)
+
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(255-bin_clo, connectivity=4)
+    # num_labels
+    # stats: x、y、width、height, area
+    # centroids: centers of components
+    pieces = []
+    output = np.zeros((img_overlap.shape[0], img_overlap.shape[1], 3), np.uint8)
+    for i in range(1, num_labels):
+        if stats[i][4] < img_overlap.shape[0] * img_overlap.shape[1] * 0.003:
+            continue
+        m = labels == i
+        pieces.append(m)
+        output[:, :, 0][m] = np.random.randint(0, 255)
+        output[:, :, 1][m] = np.random.randint(0, 255)
+        output[:, :, 2][m] = np.random.randint(0, 255)
+    return pieces, output
 
 def crop_piece(piece, img_balanced, margin=10):
     piece = piece.copy().astype(np.uint8)
@@ -21,11 +160,10 @@ def crop_piece(piece, img_balanced, margin=10):
 #     print(minY,maxY, minX,maxX)
     return piece[minX:maxX, minY:maxY], img_balanced[minX:maxX, minY:maxY]
 
-def refine_piece(piece, image_area=None, n_polys=150):
+def get_smooth_contour(piece, image_area=None, n_polys=150):
     piece = piece.copy()
     piece = cv2.dilate(piece, None, iterations=3)
     piece = cv2.erode(piece, None, iterations=3)
-    mask = np.zeros(piece.shape, dtype = np.uint8)
     contour_info = [(c, cv2.contourArea(c),) for c in cv2.findContours(piece, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[0]]
     if image_area is None:
         image_area = piece.shape[0] * piece.shape[1]
@@ -38,12 +176,18 @@ def refine_piece(piece, image_area=None, n_polys=150):
             x,y = contour.T
             x = x.tolist()[0]
             y = y.tolist()[0]
-            tck, u = splprep([x,y], u=None, s=1.0, per=1)
+            tck, u = splprep(np.array([x,y]), u=None, s=1.0, per=0)
             u_new = np.linspace(u.min(), u.max(), n_polys)
             x_new, y_new = splev(u_new, tck, der=0)
             res_array = [[[int(i[0]), int(i[1])]] for i in zip(x_new,y_new)]
-            smoothened.append(np.asarray(res_array, dtype=np.int32))
-            mask = cv2.fillPoly(mask, smoothened, (255))
+            smoothened = np.asarray(res_array, dtype=np.int32)
+            continue
+    return smoothened
+
+def refine_piece(piece, image_area=None, n_polys=150):
+    mask = np.zeros(piece.shape, dtype = np.uint8)
+    smoothened = get_smooth_contour(piece, image_area=image_area, n_polys=n_polys)
+    mask = cv2.fillPoly(mask, [smoothened], (255))
     return mask
 
 def get_vector(p1,p2):
